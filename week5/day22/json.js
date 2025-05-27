5b666656e35fdc887136ddb70cec792b40218381
Bimash Maharjan <87689921+bmasmhj@users.noreply.github.com>
2025-05-27 19:06:08 +0545

    day 22

---
+++ week5/day22/json.js
@@ -0,0 +1,49 @@
+json 
+
+// Javascript Object Notation
+// object 
+
+//  key : value 
+/*  
+
+let boyname = "John";
+let boyage = 30;
+let boycity = "New York";
+
+let boy ={
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+
+let boys = [
+{
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+},
+{
+    "name": "Mike",
+    "age": 25,
+    "city": "Los Angeles"
+},
+{
+    "name": "Tom",
+    "age": 35,
+    "city": "Chicago"
+}
+
+]
+
+let animal = [ "cat" , "dog", "fish", "bird" ]
+let mamrks = [ 1 , 2, 3, 4, 5 ]
+let fruits = [ "apple", "banana", 234, "grape" ]
+
+
+marks[3]
+boy["name"]
+
+boys[1].name
+boys[1]["name"]
+
+*/
