9efcd56caa474759d54c3496c377aa717d9fb76b
Bimash Maharjan <87689921+bmasmhj@users.noreply.github.com>
2025-05-30 19:05:37 +0545

    day 26

---
+++ week6/day26/script.js
@@ -0,0 +1,38 @@
+let school = {
+    name : "Greenwood High",
+    location : "New York",
+    classes : [
+        {
+            name : "Class 1",
+            section : [ "A", "B", "C" ],
+            students : [
+                {
+                    name: "Alice",
+                    age: 6,
+                    city: "New York"
+                },
+                {
+                    name: "Bob",
+                    age: 7,
+                    city: "Los Angeles"
+                }
+            ]
+        },
+        {
+            name : "Class 2",
+            section : [ "A", "B" ],
+            students : [
+                {
+                    name: "Charlie",
+                    age: 8,
+                    city: "Chicago"
+                },
+                {
+                    name: "David",
+                    age: 9,
+                    city: "Houston"
+                }
+            ]
+        }
+    ]
+}
